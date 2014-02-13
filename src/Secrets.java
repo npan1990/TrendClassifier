6385534b0be399ea6f22a264cc143bd49840bcae
Panos Sakkos <panos.sakkos@gmail.com>
2014-02-13 13:15:49 +0200

    authenticate

---
+++ src/Secrets.java
@@ -0,0 +1,29 @@
+/**
+ * Created by panossakkos on 2/13/14.
+ */
+
+import java.io.*;
+
+public class Secrets {
+
+    private static final String SECRETS_FOLDER = "Secrets/";
+    private static final String CONSUMER = "CONSUMER";
+    private static final String ACCESS_TOKEN = "ACCESS_TOKEN";
+
+    public static String getConsumerSecret () throws Exception {
+        BufferedReader br = new BufferedReader(new FileReader(Secrets.SECRETS_FOLDER + Secrets.CONSUMER));
+        StringBuilder sb = new StringBuilder();
+        String consumer = br.readLine();
+        br.close();
+        return consumer;
+    }
+
+    public static String getAccessTokenSecret () throws Exception {
+        BufferedReader br = new BufferedReader(new FileReader(Secrets.SECRETS_FOLDER + Secrets.ACCESS_TOKEN));
+        StringBuilder sb = new StringBuilder();
+        String accessToken = br.readLine();
+        br.close();
+        return accessToken;
+    }
+
+}
--- src/TweetsCrawler.java
+++ src/TweetsCrawler.java
@@ -1,5 +1,32 @@
 /**
  * Created by panossakkos on 2/12/14.
  */
-public class TweetsCrawler {
+
+import twitter4j.*;
+import twitter4j.auth.AccessToken;
+
+public class TweetsCrawler extends Thread {
+
+    private static final String CONSUMER_KEY = "ERLRoYHJSuCuMn2iG8Z2w";
+    private static String ACCESS_TOKEN = "40974174-Jy3bKCgVykJM6i0eN27f02CS0liqWkLTd8TdAsEaR";
+
+    private Twitter twitter;
+
+    public TweetsCrawler () throws Exception {
+        twitter = new TwitterFactory().getInstance();
+        twitter.setOAuthConsumer(CONSUMER_KEY, Secrets.getConsumerSecret());
+        AccessToken oathAccessToken = new AccessToken(ACCESS_TOKEN, Secrets.getAccessTokenSecret());
+        twitter.setOAuthAccessToken(oathAccessToken);
+
+    }
+
+    @Override public void run () {
+
+    }
+
+    public static void main(String[] args) throws Exception {
+        TweetsCrawler crawler = new TweetsCrawler();
+
+        crawler.start();
+    }
 }
