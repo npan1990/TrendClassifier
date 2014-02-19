d7e5ae0a5b9dc31d40a4203f2d69844f8bba61e6
Panos Sakkos <panos.sakkos@gmail.com>
2014-02-19 17:10:46 +0200

    Added package.

--- TrendsProcessing/src/di/kdd/trends/classifier/trends/processing/Application.java
+++ TrendsProcessing/src/di/kdd/trends/classifier/trends/processing/Application.java
@@ -1,10 +1,12 @@
+package di.kdd.trends.classifier.trends.processing;
+
 /**
  * Created by panossakkos on 2/19/14.
  */
+
 public class Application {
 
     public static void main(String []args) {
 
     }
-
 }
---
+++ TrendsProcessing/src/di/kdd/trends/classifier/trends/processing/TrendsProcessor.java
@@ -0,0 +1,9 @@
+package di.kdd.trends.classifier.trends.processing;
+
+/**
+ * Created by panossakkos on 2/19/14.
+ */
+
+public class TrendsProcessor {
+
+}
