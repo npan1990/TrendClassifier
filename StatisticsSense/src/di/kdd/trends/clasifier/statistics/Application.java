245004000694bf4a5b2181089c96871ab6b05bf7
Panos Sakkos <panos.sakkos@gmail.com>
2014-02-25 14:04:13 +0200

    Load trends.

--- StatisticsSense/src/di/kdd/trends/clasifier/statistics/Application.java
+++ StatisticsSense/src/di/kdd/trends/clasifier/statistics/Application.java
@@ -5,13 +5,21 @@ package di.kdd.trends.clasifier.statistics;
  */
 public class Application {
 
+    private static String DATA_FOLDER = "Data/";
+    private static String TRENDS_FILE = "/trends";
+    private static String TWEETS_FILE = "/tweets.tok";
+
     public static void main (String []args) throws Exception {
 
         if (args.length < 2) {
-            System.err.println("Proccesed tweets file and trends file are needed as arguments");
+            System.err.println("Processed tweets file and trends file are needed as arguments");
             return;
         }
 
-        Statistics.sense(args[0], args[1]);
+        String location = args[0];
+        String date = args[1];
+
+        Statistics.sense(Application.DATA_FOLDER + location + "/" + date + Application.TWEETS_FILE,
+                        Application.DATA_FOLDER + location + "/" + date + Application.TRENDS_FILE);
     }
 }
--- StatisticsSense/src/di/kdd/trends/clasifier/statistics/Statistics.java
+++ StatisticsSense/src/di/kdd/trends/clasifier/statistics/Statistics.java
@@ -1,12 +1,42 @@
 package di.kdd.trends.clasifier.statistics;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+
 /**
  * Created by panos on 2/25/14.
  */
+
 public class Statistics {
+
+    private static ArrayList<String> trends = new ArrayList<String>();
 
     public static void sense (String tweetsFileName, String trendsFileName) throws Exception {
 
+        Statistics.loadTweets(tweetsFileName);
+        Statistics.loadTrends(trendsFileName);
+
+        Statistics.trends.clear();
+    }
+
+    private static void loadTweets(String tweetsFileName) throws Exception {
+    }
+
+    private static void loadTrends(String trendsFileName) throws Exception {
+        BufferedReader reader = new BufferedReader(new FileReader(trendsFileName));
+
+        String line;
+
+        while ((line = reader.readLine()) != null) {
+            for (int i = 0; i < 10; i++) {
+                String trend = reader.readLine().toLowerCase().replace("#", "");
+
+                if (Statistics.trends.contains(trend) == false) {
+                    Statistics.trends.add(trend);
+                }
+            }
+        }
     }
 
 }
