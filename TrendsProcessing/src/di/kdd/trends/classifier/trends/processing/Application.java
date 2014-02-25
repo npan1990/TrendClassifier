e1e3e4772ac99c7aab3de2c6a939de554f35b396
Panos Sakkos <panos.sakkos@gmail.com>
2014-02-25 12:37:38 +0200

    Added command line arguments in order to handle the trends processor.

--- TrendsProcessing/src/di/kdd/trends/classifier/trends/processing/Application.java
+++ TrendsProcessing/src/di/kdd/trends/classifier/trends/processing/Application.java
@@ -6,12 +6,21 @@ package di.kdd.trends.classifier.trends.processing;
 
 public class Application {
 
+    private static String TREND_FILE = "trends";
     private static String DATA_FOLDER = "Data/";
 
     public static void main(String []args) throws Exception {
-        TrendsProcessor trendsProcessor = new TrendsProcessor();
-        trendsProcessor.process(Application.DATA_FOLDER + "trends-18-02");
 
-        return;
+        if (args.length == 0) {
+            System.err.println("Location and date are needed as arguments.");
+            return;
+        }
+
+        String location = args[0];
+        String date = args[1];
+
+        TrendsProcessor trendsProcessor = new TrendsProcessor();
+        trendsProcessor.process(Application.DATA_FOLDER + location + "/" + date + "/" + Application.TREND_FILE);
+
     }
 }
